8363d34b44320a91b64e60c5f929023493d30ecf
alvarikola <148550896+alvarikola@users.noreply.github.com>
2024-10-21 08:16:03 +0100

    Update HilosAdivinadores.java

--- HilosAdivinadores/src/hilosadivinadores/HilosAdivinadores.java
+++ HilosAdivinadores/src/hilosadivinadores/HilosAdivinadores.java
@@ -3,7 +3,7 @@ package hilosadivinadores;
 public class HilosAdivinadores {
 
     public static void main(String[] args) {
-        NumeroOculto numeroOculto = new NumeroOculto(); // Instancia del número oculto.
+        NumeroOculto numeroOculto = new NumeroOculto();
 
         // Crear y ejecutar los 10 hilos adivinadores.
         for (int i = 1; i <= 10; i++) {
